3abd0dc47d348d7c3ba2fd363adf307329e1520e
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-06 04:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const app = "I don't do much."
 var kittens = ["Milo","Otis","Garfield"];
 
 function destructivelyPrependKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyAppendKitten(name) {
   
 }
